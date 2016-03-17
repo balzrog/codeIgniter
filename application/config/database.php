76c1392e1174e7d8c7452b0d380fbef228e2312d
Guillaume <guillaume.goisset@viacesi.fr>
2016-03-17 10:36:19 +0100

    Modification session data

--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array( 
+$db['default'] = array(
 	'dsn'	=> 'mysql:host=164.138.29.106; dbname=eportfolio; charset=utf8;',
 	'hostname' => '',
 	'username' => 'dgls',
