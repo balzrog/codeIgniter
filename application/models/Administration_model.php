d11e0fbab9b76c428280a6c0f6acc8bde54e2689
balzrog <adrien.laffargue@viacesi.fr>
2016-03-18 14:25:05 +0100

    Admin

---
+++ application/models/Administration_model.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Balzrog
+ * Date: 18/03/2016
+ * Time: 14:24
+ */
+class Administration_model extends CI_Model
+{
+
+}
---
+++ application/views/Administration_view.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Balzrog
+ * Date: 18/03/2016
+ * Time: 14:24
+ */
+
+echo "Administration";
