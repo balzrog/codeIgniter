22be654b6565ecaf22b03d030383524c6eb31a7a
Nicolas Dubois <nicolas.dubois1@viacesi.fr>
2016-03-18 15:30:57 +0100

    déplacement du container dans les pages

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -8,6 +8,21 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<div class="col-xs-12 col-lg-10">
-    <h1>Connexion</h1>
-</div>
+<section class="container">
+    <div class="col-xs-12 col-lg-10">
+        <h1>DGLS PortFolio</h1>
+        <p>
+            Bienvenue sur le site DGLS PortFolio, un site de création de porte-folios personnalisés, veuillez choisir si vous êtes employeur ou à la recherche d'un emploi.
+        </p>
+        <div "seeker_choice">
+            <p>Vous êtes demandeur d'emploi?</p>
+            <button type="button" class="btn btn-secondary">S'identifier</button>
+            <button type="button" class="btn btn-secondary">S'inscrire</button>
+        </div>
+        <div "offerer_choice">
+            <p>Vous êtes offreur d'emploi?</p>
+            <input >
+            <button type="button" class="btn btn-secondary"><span class="fa fa-search"></span></button>
+        </div>
+    </div>
+</section>
